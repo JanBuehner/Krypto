0aa8849cd38c80de05ae5c05ff8e1281d49c8182
Jan Bühner <jan.buehner01@gmail.com>
2018-09-10 15:17:47 +0200

    Gemeinsames Projekt angelegt

---
+++ Caesar.java
@@ -0,0 +1,90 @@
+
+/**
+ * Write a description of class Caesar here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Caesar
+{
+    public Caesar()
+    {
+        
+    }
+    
+    public String codieren(String pEingabe, int pSchluessel)
+    {
+        String eingabe = pEingabe.toUpperCase();
+        String codiert = "";
+        int aktBuchstabe = 0; 
+        
+        for( int i = 0; i < eingabe.length(); i++)
+        {
+            aktBuchstabe = eingabe.charAt(i) + pSchluessel;
+            if(aktBuchstabe > 90)
+            {
+                int hHelp = aktBuchstabe - 90;
+                aktBuchstabe = 64 + hHelp;
+                codiert = codiert + (char) aktBuchstabe;
+            }
+            else 
+            {
+                codiert = codiert + (char) aktBuchstabe;
+            }
+        }
+        System.out.println(codiert);
+        return codiert;
+    }
+    
+    public String decodieren(String pEingabe, int pSchluessel)
+    {
+        String eingabe = pEingabe.toUpperCase();
+        String decodiert = "";
+        int aktBuchstabe = 0; 
+        
+        for( int i = 0; i < eingabe.length(); i++)
+        {
+            aktBuchstabe = eingabe.charAt(i) - pSchluessel;
+            if(aktBuchstabe < 65)
+            {
+                int hHelp = 65 - aktBuchstabe;
+                aktBuchstabe = 91 - hHelp;
+                decodiert = decodiert + (char) aktBuchstabe;
+            }
+            else 
+            {
+                decodiert = decodiert + (char) aktBuchstabe;
+            }
+        }
+        return decodiert;
+    }
+    
+    public void brutalForce(String pEingabe)
+    {
+        String eingabe = pEingabe.toUpperCase();
+        String decodiert = "";
+        int aktBuchstabe = 0; 
+        
+        for(int i = 0; i < 26; i++)
+        {
+            for( int j = 0; j < eingabe.length(); j++)
+            {
+                aktBuchstabe = eingabe.charAt(j) - i;
+                if(aktBuchstabe < 65)
+                {
+                    int hHelp = 65 - aktBuchstabe;
+                    aktBuchstabe = 91 - hHelp;
+                    decodiert = decodiert + (char) aktBuchstabe;
+                }
+                else 
+                {
+                    decodiert = decodiert + (char) aktBuchstabe;
+                }
+            }
+            System.out.println(decodiert + " bei Schluessel " + i);
+            decodiert = "";
+        }
+    }
+    
+    
+}
